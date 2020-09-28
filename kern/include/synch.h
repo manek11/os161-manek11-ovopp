f76b5a8872789efc99f22f3d9aed7293d9ec5752
manek11 <manekgujral11@gmail.com>
2020-09-28 09:00:52 -0400

    Asst 2 completed

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -119,6 +119,11 @@ bool lock_do_i_hold(struct lock *);
 
 struct cv {
         char *cv_name;
+        /////
+        struct wchan *cv_wchan;
+	    struct spinlock cv_lock;
+	    //volatile bool cv_sig;
+        /////
         // add what you need here
         // (don't forget to mark things volatile as needed)
 };
