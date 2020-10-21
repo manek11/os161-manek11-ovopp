5ef7308d85711e07ac5edfe6ee37300626d17a76
manek11 <manekgujral11@gmail.com>
2020-10-21 11:19:33 -0400

    All tests passed

--- kern/syscall/file_table_syscall.c
+++ kern/syscall/file_table_syscall.c
@@ -63,50 +63,7 @@ sys_open(const char *filename, int flags, mode_t mode, int32_t * retval){
 ssize_t
 sys_read(int fd, void *buf , size_t buflen, int32_t * retval){
    
-   
-   /* if(fd < 0 || fd >= OPEN_MAX){
-        return EBADF;
-        }
-    
-    if (buf == NULL){
-        return EFAULT;
-    } 
-    
-    if (curproc->file_table_arr[fd].ft_vnode == NULL){
-        return EBADF;
-    }    
-    
-    if (buflen <= 0){
-        return EFAULT;
-    }
-    
-     if (!(curproc->file_table_arr[fd].flag & O_ACCMODE)){
-            return EBADF;
-        }
-        
-    //}
-    if(curproc->file_table_arr[fd].ft_vnode != NULL && curproc->file_table_arr[fd].flag != O_WRONLY){
-        void *kbuf = kmalloc(sizeof(void*));
-        struct uio myuio;
-        struct iovec myiovec;
-        uio_kinit(&myiovec, &myuio, kbuf, buflen, curproc->file_table_arr[fd].offset, UIO_READ);
-        
-        int residbefore = myuio.uio_resid;
-        int ret = VOP_READ(curproc->file_table_arr[fd].ft_vnode, &myuio);
-        if(ret)
-            return ret;
-        curproc->file_table_arr[fd].offset = myuio.uio_offset;
-        int ret2 = copyout(kbuf,(userptr_t) buf, buflen); //at this point we read the file from FD to the buffer in userspace
-        if(ret2){
-            return EFAULT;
-        }
-        *retval = residbefore - myuio.uio_resid;
-        return 0; // return the number of bytes 
-    }
-    else{
-        return EBADF;
-    }*/
-    
+
      if(fd < 0 || fd >= OPEN_MAX){
         return EBADF;
      }
@@ -192,7 +149,11 @@ sys_write(int fd, const void *buffer, size_t nbytes, int32_t * retval){
 
 off_t
 sys_lseek(int fd, off_t pos, int whence, int64_t * retval){
-    if(fd < 0 || fd > OPEN_MAX){
+    if(fd < 0 || fd >= OPEN_MAX){
+        return EBADF;
+    }
+    
+    if (curproc->file_table_arr[fd].ft_vnode == NULL){
         return EBADF;
     }
 
@@ -200,10 +161,6 @@ sys_lseek(int fd, off_t pos, int whence, int64_t * retval){
     struct stat mystat;
     off_t seek = curproc->file_table_arr[fd].offset;
     
-    if(!VOP_ISSEEKABLE(curproc->file_table_arr[fd].ft_vnode)){
-        return ESPIPE;
-    }
-
     switch (whence)
     {
         case SEEK_SET:
@@ -228,6 +185,13 @@ sys_lseek(int fd, off_t pos, int whence, int64_t * retval){
         return EINVAL;
     }
     curproc->file_table_arr[fd].offset = seek;
+    
+        
+    if(!VOP_ISSEEKABLE(curproc->file_table_arr[fd].ft_vnode)){
+        return ESPIPE;
+    }
+    
+    
     *retval = curproc->file_table_arr[fd].offset;
     return 0;
 };
@@ -235,7 +199,7 @@ sys_lseek(int fd, off_t pos, int whence, int64_t * retval){
 
 int
 sys_close(int fd){
-    if(fd < 0 || fd > OPEN_MAX){
+    if(fd < 0 || fd >= OPEN_MAX){
         return EBADF;
     }
     if(curproc->file_table_arr[fd].ft_vnode != NULL){
@@ -262,23 +226,23 @@ sys_close(int fd){
 
 int
 sys_dup2(int oldfd, int newfd, int32_t * retval){
-    if(oldfd < 0 || newfd < 0){
+    if(oldfd < 0 || oldfd >= OPEN_MAX){
         return EBADF;
     }
-    if(oldfd > OPEN_MAX || newfd > OPEN_MAX){
-       return EMFILE; 
+    if( newfd < 0 || newfd >= OPEN_MAX){
+       return EBADF; 
     }
+    
+    if (curproc->file_table_arr[oldfd].ft_vnode == NULL){
+        return EBADF;
+    }
+    
     if(oldfd == newfd){
-        *retval = newfd;
+       *retval = newfd;
         return 0;
     }
     
     if(curproc->file_table_arr[newfd].ft_vnode == NULL){
-        if(curproc->file_table_arr[oldfd].ft_vnode == NULL){
-            *retval = newfd;
-            return 0;
-        }
-        else{
         // copy over vnode pointer and all the flags. both FDs now point to same pointer
             curproc->file_table_arr[newfd].ft_vnode = curproc->file_table_arr[oldfd].ft_vnode;
             curproc->file_table_arr[newfd].offset = curproc->file_table_arr[oldfd].offset;
@@ -286,9 +250,11 @@ sys_dup2(int oldfd, int newfd, int32_t * retval){
             *retval = newfd;
             return 0;
         }
-    }
     else if(curproc->file_table_arr[newfd].ft_vnode != NULL){
-        return sys_close(newfd);
+        int ret_v = sys_close(newfd);
+        if (ret_v){
+            return ret_v;
+        }
     }
     
     return EBADF;
